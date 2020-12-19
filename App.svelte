2a60303648cebc71b6e4a9f7fa6f8717a5bef78b
Garth Braithwaite <garthdb@gmail.com>
2020-12-19 13:41:15 -0700

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Button from "./Button.svelte";
+  import FormField from "./FormField.svelte";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>Hello CodeSandbox</h1>
+	<h2>Start editing to see some magic happen!</h2>
+	<Button />
+  <FormField label="Name"/>
+</main>
---
+++ FormField.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let label = "";
+  export let type = "text";
+</script>
+
+<label>
+  {label}
+  <input type={type}/>
+</label>
